b4d0d79a4a7586161fbc56a3da9779f30599556b
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-10-29 10:40:03 +0800

    compile

--- include/Algorithms/BreadthFirstSearcher.h
+++ include/Algorithms/BreadthFirstSearcher.h
@@ -3,6 +3,7 @@
 
 #include <functional>
 #include <optional>
+#include <bits/stdc++.h>
 
 template <typename TGraph>
 class BreadthFirstSearcher {
@@ -10,5 +11,42 @@ class BreadthFirstSearcher {
   static void VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action);
   static std::optional<int> FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate);
 };
+
+template <typename TGraph>
+void BreadthFirstSearcher<TGraph>::VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action){
+  if(!graph->ContainsVertex(start))return;
+  std::map<int,bool> visit;
+  std::queue<int> q;
+  while(q.size()){
+    int now = q.front(); q.pop();
+    if(visit.find(now) != visit.end()) continue;
+    action(now);
+    visit.insert({now, true});
+    std::vector<int> to = graph->GetNeighbors(now);
+    for(auto it: to){
+      if(visit.find(it) != visit.end()) continue;
+      q.push(it);
+    }
+  }
+}
+
+template <typename TGraph>
+std::optional<int> BreadthFirstSearcher<TGraph>::FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate){
+  if(!graph->ContainsVertex(start))return std::nullopt;
+  std::map<int,bool>visit;
+  std::queue<int>q;
+  while(q.size()){
+    int now = q.front(); q.pop();
+    if(visit.find(now) != visit.end()) continue;
+    if(predicate(now)) return (std::optional<int>) now;
+    visit.insert({now, true});
+    std::vector<int> to = graph->GetNeighbors(now);
+    for(auto it: to){
+      if(visit.find(it) != visit.end()) continue;
+      q.push(it);
+    }
+  }
+  return std::nullopt;
+}
 
 #endif
--- include/Algorithms/DepthFirstSearcher.h
+++ include/Algorithms/DepthFirstSearcher.h
@@ -3,12 +3,56 @@
 
 #include <functional>
 #include <optional>
+#include <bits/stdc++.h>
 
 template <typename TGraph>
 class DepthFirstSearcher {
- public:
-  static void VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action);
-  static std::optional<int> FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate);
+  public:
+    static void VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action);
+    static std::optional<int> FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate);
+  private:
+    static void dfsAction(const TGraph *graph, int now, std::map<int,bool> *visit, std::function<void(int)> action);
+    static std::optional<int> dfsPredicate(const TGraph *graph, int now, std::map<int,bool> *visit, std::function<bool(int)> predicate);
+    
 };
 
+template <typename TGraph>
+void DepthFirstSearcher<TGraph>::dfsAction(const TGraph *graph, int now, std::map<int,bool> *visit, std::function<void(int)> action){
+  visit->insert({now, true});
+  action(now);
+  std::vector<int> to = graph->GetNeighbors(now);
+  for(auto it : to){
+    if(visit->find(it) != visit->end()) continue;
+    dfsAction(graph, it, visit, action);
+  }
+  return;
+}
+
+template <typename TGraph>
+std::optional<int> DepthFirstSearcher<TGraph>::dfsPredicate(const TGraph *graph, int now, std::map<int,bool> *visit, std::function<bool(int)> predicate){
+  visit->insert({now, true});
+  if(predicate(now))return (std::optional<int>) now;
+  std::vector<int> to = graph->GetNeighbors(now);
+  for(auto it : to){
+    if(visit->find(it) != visit->end())continue;
+    std::optional<int> res = dfsPredicate(graph, it, visit, predicate);
+    if(res != std::nullopt)
+      return res;
+  }
+  return std::nullopt;
+}
+
+template <typename TGraph>
+void DepthFirstSearcher<TGraph>::VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action){
+  if(!graph->ContainsVertex(start))return;
+  std::map<int,bool>visit;
+  dfsAction(graph, start, &visit, action);
+}
+
+template <typename TGraph>
+std::optional<int> DepthFirstSearcher<TGraph>::FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate){
+  if(!graph->ContainsVertex(start))return std::nullopt;
+  std::map<int,bool>visit;
+  return dfsPredicate(graph, start, &visit, predicate);
+}
 #endif
