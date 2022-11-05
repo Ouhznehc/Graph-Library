3d7420dde9cbf0c21d9bb077410b646ef66b17e6
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-05 19:31:03 +0800

    compile

--- include/DataStructures/WeightedGraph.h
+++ include/DataStructures/WeightedGraph.h
@@ -26,9 +26,8 @@ class WeightedGraph : public Graph
 
     public:
         weightType GetWeight(int vertex1, int vertex2) const{
-            if(WeightedEdges.find(WeightedEdge<weightType>(vertex1, vertex2, 0)) == WeightedEdges.end()) return weightType();
-            else return weightType();
-            //else return WeightedEdges.find(WeightedEdge<weightType>(vertex1, vertex2, 0))->GetWeight();
+            if(WeightedEdges.find(WeightedEdge<weightType>(vertex1, vertex2, weightType())) == WeightedEdges.end()) return weightType();
+            else return WeightedEdges.find(WeightedEdge<weightType>(vertex1, vertex2, 0))->GetWeight();
         }
 
         std::vector<WeightedEdge<weightType>> GetEdges() const{
