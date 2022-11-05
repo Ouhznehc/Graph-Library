ac6f49540a1f4ae52cea8eb12ab8e8cfb39aee66
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-05 15:41:09 +0800

    compile

--- include/Algorithms/DijkstraShortestPaths.h
+++ include/Algorithms/DijkstraShortestPaths.h
@@ -13,22 +13,19 @@ class DijkstraShortestPaths : public ShortestPaths<TGraph, TValue>
         , dijkstra()
         {
             if(this->graph->ContainsVertex(this->source)) return;
-            unordered_multiset<int> visit;
             while(dijkstra.size()) dijkstra.pop();
             this->distance.clear();
             this->pi.clear();
             this->distance[this->source] = TValue();
-            visit.insert(this->source);
             dijkstra.push(node(this->source, this->distance[this->source]));
             while(dijkstra.size()){
                 auto now  = dijkstra.top().index; dijkstra.pop();
                 for(auto to: this->graph->GetOutgoingEdges(now)){
                     int idx = to.GetDestination();
                     TValue dist = this->distance[now] + to.GetWeight();
-                    if((visit.find(idx) == visit.end()) || dist < this->distance[idx]){
+                    if((distance.find(idx) == distance.end()) || dist < this->distance[idx]){
                         this->distance[idx] = dist;
                         this->pi[idx] = now; 
-                        visit.insert(idx);
                         dijkstra.push(node(idx, dist));
                     }
                 }
