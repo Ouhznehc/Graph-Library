a60608f21b22bc30c7d96545499528cb61813b84
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-05 13:35:02 +0800

    compile

---
+++ include/Algorithms/DijkstraShortestPaths.h
@@ -0,0 +1,33 @@
+#ifndef DIJKSTRA_SHORTEST_PATHS
+#define DIJKSTRA_SHORTEST_PATHS
+
+#include <bits/stdc++.h>
+#include <ShortestPaths.h>
+
+
+template <template<typename> class TGraph, typename TValue>
+class DijkstraShortestPaths : public ShortestPaths<TGraph, TValue>
+{
+    public:
+        DijkstraShortestPaths(const TGraph<TValue> *graph, int source)
+        : ShortestPaths<TGraph, TValue>(graph, source)
+        , dijkstra()
+        {
+            if(!graph->ContainsVertex(source)) return;
+            
+        }
+    private:
+        struct node 
+        {  
+            int index; 
+            TValue value;
+            node(int index = int(), TValue value = TValue())
+            : index(index), value(value) {}
+            bool operator< (const node &b) const {return value < b.value;}
+        };
+        std::priority_queue<node> dijkstra;
+        virtual bool HasPathTo(int destination) const;
+};
+
+
+#endif
