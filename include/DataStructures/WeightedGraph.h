3bce301c8a887a8834b4d50fcbb8f3c5439fffa5
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-05 19:43:01 +0800

    compile

--- include/DataStructures/WeightedGraph.h
+++ include/DataStructures/WeightedGraph.h
@@ -27,7 +27,7 @@ class WeightedGraph : public Graph
     public:
         weightType GetWeight(int vertex1, int vertex2) const{
             if(!ContainsEdge(vertex1, vertex2)) return weightType();
-            else return 1;
+            else return weightType();
             //WeightedEdges.find(WeightedEdge<weightType>(vertex1, vertex2, weightType()))->GetWeight();
         }
 
