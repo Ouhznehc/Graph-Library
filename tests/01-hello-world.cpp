49d01c395cc4e3641783895b90ca9c252b34f1b1
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-23 17:27:50 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -10,38 +10,6 @@
 
 using namespace std;
 
-struct sgsgsg{
-  int a;
-  sgsgsg(){}
-  sgsgsg(int _a){a=_a;}
-  bool operator > (const sgsgsg &b) const{return false;}
-};
-
-static void test1() {
-
-  auto *g = new WeightedGraph<sgsgsg>();
-  for (int i = 1; i <= 6; ++i) {
-    g->AddVertex(i);
-  }
-  g->AddEdge(1, 2, 1);
-  g->AddEdge(2, 3, 2);
-  g->AddEdge(3, 4, 3);
-  g->AddEdge(4, 1, 4);
-  g->AddEdge(5, 6, 5);
-  g->AddEdge(6, 5, 6);
-
-  ShortestPaths<WeightedGraph<sgsgsg>> *p = nullptr;
-  for (int i = 1; i <= 6; ++i) {
-    p = new BellmanFordShortestPaths<WeightedGraph<sgsgsg>>(g, i);
-    for (int j = 1; j <= 6; ++j) {
-      printf("%d", p->HasPathTo(j));
-    }
-    printf("\n");
-    delete p;
-  }
-
-  delete g;
-}
 struct strict_int {
     int val;
     strict_int(int v): val(v) {}
@@ -49,8 +17,9 @@ struct strict_int {
 };
 static WeightedGraph<strict_int> test_wg1;
 static UndirectedWeightedGraph<strict_int> test_wg2;
+static ShortestPaths<UndirectedWeightedGraph<strict_int>> a;
+
 int main()
 {
-   test1();
    return 0;
 }
