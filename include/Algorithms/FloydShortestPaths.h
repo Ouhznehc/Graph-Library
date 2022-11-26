ebff632a2969aa4f4c4e8a2bb3728685364bbca7
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-26 15:44:27 +0800

    compile

--- include/Algorithms/FloydShortestPaths.h
+++ include/Algorithms/FloydShortestPaths.h
@@ -17,8 +17,9 @@ class FloydShortestPaths : public MultiSourceShortestPaths<TGraph>
                         if(!this->HasPathOf(i, mid) || !this->HasPathOf(mid, j)) continue;
                         typename TGraph ::TValue dist = this->distance.at(std::make_pair(i, mid)) + this->distance.at(std::make_pair(mid, j));
                         if(!this->HasPathOf(i, j) || dist < this->distance.at(std::make_pair(i, j))){
-                            this->distance.emplace(std::make_pair(i, j), dist);
-                            this->pi.emplace(std::make_pair(i, j), mid);
+                            // this->distance.emplace(std::make_pair(i, j), dist);
+                            // this->pi.emplace(std::make_pair(i, j), mid);
+                            this->distance[(i, j)] = dist;
                         }
                     }
         }
