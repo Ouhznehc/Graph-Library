235e91a45aea246896ff60e0e941ceb9c5f9cd7d
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-12-04 00:21:20 +0800

    compile

---
+++ include/Exceptions/NegativeCycleException.h
@@ -0,0 +1,20 @@
+#ifndef NEGATIVE_CYCLE_EXCEPTION
+#define NEGATIVE_CYCLE_EXCEPTION
+
+#include <iostream>
+#include <exception>
+#include <string>
+#include <GLException.h>
+
+class NegativeCycleException : public GLException
+{
+ protected:
+  std::string message;
+ public:
+  explicit NegativeCycleException(const std::string &s);
+  virtual std::string GetMessage() const;
+};
+
+std::ostream &operator<<(std::ostream &os, const NegativeCycleException &e);
+
+#endif
